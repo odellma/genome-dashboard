64be00877a8729e4fdf51d3e1a5568d03c4905e9
Matthew O'Dell <odell.data@gmail.com>
2026-06-21 10:38:46 -0800

    api call wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let { data }: {data: PageData } = $props();
+    console.log(data);
+</script>
+<select>
+	<option value="">Select a species...</option>
+
+	{#each data.species as species}
+		<option value={species.id}>
+			{species.commonName}
+		</option>
+	{/each}
+</select>
---
+++ src/routes/+page.ts
@@ -0,0 +1,10 @@
+import type { PageLoad } from './$types';
+import { getSpecies } from '$lib/api/species';
+
+// if your load function should always run on the server because it uses private environment variables or database access then it should go in a +page.server.js instead.
+export const load: PageLoad = async ({ fetch }) => {
+    console.log('in page load');
+    return {
+        species: await getSpecies(fetch)
+    };
+};
